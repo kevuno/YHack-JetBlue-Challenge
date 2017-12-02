17e14f1c8a562d2faaf93d0c377d870217327ce8
apatheticallyhuman <chaedonetechnical@gmail.com>
2017-12-01 23:21:37 -0500

    datetime output changed to date

--- jetblue.sql
+++ jetblue.sql
@@ -1,7 +1,7 @@
 --Basic Search Queries for Mysql
 
 -- Find Date
-SELECT Origin, Destination, FlightDate, FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", state.state_table 
+SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", state.state_table 
 FROM JetBlue JOIN state_table ON JetBlue.Destination = state_table.airport  WHERE (Origin like Query
 AND Destination like Query
 AND DollarFare <= Query
@@ -10,7 +10,7 @@ AND IsDomesticRoute = 1
 ); 
 
 -- Find Price
-SELECT Origin, Destination, FlightDate, FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", state.state_table
+SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", state.state_table
 FROM JetBlue JOIN state_table ON JetBlue.Destination = state_table.airport  WHERE (Origin like Query
 AND Destination like Query
 AND FlightDate = Query
@@ -18,7 +18,7 @@ AND IsDomesticRoute = 1
 ); 
 
 -- Find Dest
-SELECT Origin, Destination, FlightDate, FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", state.state_table
+SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", state.state_table
 FROM JetBlue JOIN state_table ON JetBlue.Destination = state_table.airport WHERE (Origin like Query
 AND FlightDate = Query
 AND DollarFare <= Query
