65e491c3ff1ebb5baf750297bde72c6dd9f3d908
apatheticallyhuman <chaedonetechnical@gmail.com>
2017-12-01 23:05:23 -0500

    search queries updated with state and cleaner output

--- jetblue.sql
+++ jetblue.sql
@@ -1,7 +1,8 @@
 --Basic Search Queries for Mysql
 
 -- Find Date
-SELECT * FROM JetBlue WHERE (Origin like Query
+SELECT Origin, Destination, FlightDate, FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", state.state_table 
+FROM JetBlue JOIN state_table ON JetBlue.Destination = state_table.airport  WHERE (Origin like Query
 AND Destination like Query
 AND DollarFare <= Query
 AND DollarFare >= Query
@@ -9,18 +10,19 @@ AND IsDomesticRoute = 1
 ); 
 
 -- Find Price
-SELECT * FROM JetBlue WHERE (Origin like Query
+SELECT Origin, Destination, FlightDate, FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", state.state_table
+FROM JetBlue JOIN state_table ON JetBlue.Destination = state_table.airport  WHERE (Origin like Query
 AND Destination like Query
 AND FlightDate = Query
 AND IsDomesticRoute = 1
 ); 
 
 -- Find Dest
-SELECT * FROM JetBlue WHERE (Origin like Query
+SELECT Origin, Destination, FlightDate, FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", state.state_table
+FROM JetBlue JOIN state_table ON JetBlue.Destination = state_table.airport WHERE (Origin like Query
 AND FlightDate = Query
 AND DollarFare <= Query
 AND DollarFare >= Query
 AND IsDomesticRoute = 1
 ); 
 
--- 
