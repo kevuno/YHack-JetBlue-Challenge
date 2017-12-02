9895c4879a6722f6f4e58940bc023fa9e81a23d3
Kevin Bastian <kjb5642@rit.edu>
2017-12-02 13:23:38 -0500

    Changed column names in jetblue.sql and added AirportLocations create query

--- jetblue.sql
+++ jetblue.sql
@@ -1,8 +1,8 @@
 --Basic Search Queries for Mysql
 
 -- Find Date
-SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", airportlocations.City 
-FROM JetBlue JOIN airportlocations ON JetBlue.Destination = airportlocations.Airport  WHERE (Origin like Query
+SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", AirportLocations.City 
+FROM JetBlue JOIN AirportLocations ON JetBlue.Destination = AirportLocations.Airport  WHERE (Origin like Query
 AND Destination like Query
 AND DollarFare <= Query
 AND DollarFare >= Query
@@ -10,16 +10,16 @@ AND IsDomesticRoute = 1
 ); 
 
 -- Find Price
-SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", airportlocations.City
-FROM JetBlue JOIN airportlocations ON JetBlue.Destination = airportlocations.Airport  WHERE (Origin like Query
+SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", AirportLocations.City
+FROM JetBlue JOIN AirportLocations ON JetBlue.Destination = AirportLocations.Airport  WHERE (Origin like Query
 AND Destination like Query
 AND FlightDate = Query
 AND IsDomesticRoute = 1
 ); 
 
 -- Find Dest
-SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", airportlocations.City
-FROM JetBlue JOIN airportlocations ON JetBlue.Destination = airportlocations.Airport WHERE (Origin like Query
+SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", AirportLocations.City
+FROM JetBlue JOIN AirportLocations ON JetBlue.Destination = AirportLocations.Airport WHERE (Origin like Query
 AND FlightDate = Query
 AND DollarFare <= Query
 AND DollarFare >= Query
