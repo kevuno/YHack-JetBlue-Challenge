6cc16c14b74254f69c519bd77250d9c138556e78
Kevin Bastian <kjb5642@rit.edu>
2017-12-02 21:12:45 -0500

    Added random daily cost of living seeding to AirportLocations

--- app/Seeder.js
+++ app/Seeder.js
@@ -26,6 +26,7 @@ function seedAirportLocations(filename){
     // When parsing, it also generates a random temperature
     let parsed_data = reader.readFile(filename, function(parsed_data){
         parsed_data = addRandomTemperature(parsed_data);
+        parsed_data = addRandomDailyCost(parsed_data);
         console.log(parsed_data);
         db.insertData("AirportLocations", con,  parsed_data);
         console.log("file: " + filename + " read and db populated correctly");
@@ -35,11 +36,21 @@ function seedAirportLocations(filename){
 // Inserts a temperature filed with a random value between -5 and 30 to every object in the data set
 function addRandomTemperature(data_set){
     data_set.forEach(function(element) {
-        element.Temperature = Math.floor(Math.random() * 39) - 1; 
+        element.Temperature = Math.floor(Math.random() * 30) - 10; 
     }, this);
     return data_set;
 
 }
+
+// Inserts a cost of  filed with a random value between -5 and 30 to every object in the data set
+function addRandomDailyCost(data_set){
+    data_set.forEach(function(element) {
+        element.DailyCost = Math.floor(Math.random() * 150) - 20; 
+    }, this);
+    return data_set;
+
+}
+
 
 module.exports.seedLowestFares = seedLowestFares;
 module.exports.seedAirportLocations = seedAirportLocations;
