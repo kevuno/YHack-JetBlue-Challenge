9c45d22575fd32064667b6544be9424317470532
Kevin Bastian <kjb5642@rit.edu>
2017-12-03 00:02:28 -0500

    Added expaples of results for a query in controller app

--- controller/app.js
+++ controller/app.js
@@ -20,8 +20,46 @@ function getAirports(search_data){
 var App = new Vue({
     el: "#main_container" ,
     data: {
-        origin: ""
-        
+        origin: "",
+        results: [{
+            Airport:"JFK",
+            City: "New York City",
+            Country: "United States",
+            DailyCost: 44,
+            Destination:"JFK",
+            DollarFare: 203,
+            DollarTax: 27,
+            FareType:"LOWEST",
+            FlightDate: "2017-11-30T10:11:00.000Z",
+            FlightType: "NONSTOP",
+            IsDomesticRoute: 1,
+            IsPrivateFare: 0,
+            Origin: "ROC",
+            PointFare: 0,
+            PointsTax:0,
+            State: "New York",
+            Temperature: 29,
+            UberOrNot: 1,
+        },{
+            Airport:"JFK",
+            City: "New York City",
+            Country: "United States",
+            DailyCost: 44,
+            Destination:"JFK",
+            DollarFare: 203,
+            DollarTax: 27,
+            FareType:"LOWEST",
+            FlightDate: "2017-11-30T10:11:00.000Z",
+            FlightType: "NONSTOP",
+            IsDomesticRoute: 1,
+            IsPrivateFare: 0,
+            Origin: "ROC",
+            PointFare: 0,
+            PointsTax:0,
+            State: "New York",
+            Temperature: 29,
+            UberOrNot: 1,
+        }]
     },
     methods: {
         perform_search: function(){
