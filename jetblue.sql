f3302aad66cb384fd460da5e22cb87e89d131953
apatheticallyhuman <chaedonetechnical@gmail.com>
2017-12-02 21:02:24 -0500

    new sql

--- jetblue.sql
+++ jetblue.sql
@@ -26,3 +26,39 @@ AND DollarFare >= Query
 AND IsDomesticRoute = 1
 ); 
 
+--Advanced Search Options (Filters)
+
+-- Find Date | Language
+SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", AirportLocations.City 
+FROM JetBlue JOIN AirportLocations ON JetBlue.Destination = AirportLocations.Airport 
+JOIN CountryLanguage ON AirportLocations.Country = CountryLanguage.Country
+WHERE (Origin like Query
+AND Destination like Query
+AND DollarFare <= Query
+AND DollarFare >= Query
+AND IsDomesticRoute = 1
+AND CountryLanguage.Language like '%query%'
+); 
+
+-- Find Price | Language
+SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", AirportLocations.City
+FROM JetBlue JOIN AirportLocations ON JetBlue.Destination = AirportLocations.Airport 
+JOIN CountryLanguage ON AirportLocations.Country = CountryLanguage.Country
+WHERE (Origin like Query
+AND Destination like Query
+AND FlightDate = Query
+AND IsDomesticRoute = 1
+AND CountryLanguage.Language like '%query%'
+); 
+
+-- Find Dest | Language
+SELECT Origin, Destination, DATE(FlightDate), FlightType, DollarFare, DollarTax, DollarFare + DollarTax as "FinalFare", AirportLocations.City
+FROM JetBlue JOIN AirportLocations ON JetBlue.Destination = AirportLocations.Airport 
+JOIN CountryLanguage ON AirportLocations.Country = CountryLanguage.Country
+WHERE (Origin like Query
+AND FlightDate = Query
+AND DollarFare <= Query
+AND DollarFare >= Query
+AND IsDomesticRoute = 1
+AND CountryLanguage.Language like '%query%'
+); 
